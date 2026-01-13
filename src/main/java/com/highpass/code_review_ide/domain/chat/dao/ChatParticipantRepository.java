5825c2dec3b4d6be494bd0345e0931b2f486408b
sjmoon00 <paulmoon00@naver.com>
2026-01-13 19:23:47 +0900

    fix: 메서드 수정

--- src/main/java/com/highpass/code_review_ide/domain/chat/dao/ChatParticipantRepository.java
+++ src/main/java/com/highpass/code_review_ide/domain/chat/dao/ChatParticipantRepository.java
@@ -12,23 +12,13 @@ import org.springframework.data.repository.query.Param;
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
     List<ChatParticipant> findByChatRoom(ChatRoom chatRoom);
 
-    boolean existsByChatRoomAndMember(ChatRoom chatRoom, User user);
+    boolean existsByChatRoomAndUser(ChatRoom chatRoom, User user);
 
-    List<ChatParticipant> findByMember(User user);
+    List<ChatParticipant> findByUser(User user);
 
-    Optional<ChatParticipant> findByChatRoomAndMember(ChatRoom chatRoom, User user);
+    Optional<ChatParticipant> findByChatRoomAndUser(ChatRoom chatRoom, User user);
 
     long countByChatRoom(ChatRoom chatRoom);
 
-    @Query("""
-            SELECT cp1.chatRoom 
-            FROM ChatParticipant cp1 
-            JOIN ChatParticipant cp2 
-                ON cp1.chatRoom.id = cp2.chatRoom.id 
-            WHERE cp1.member.id = :memberId 
-              AND cp2.member.id = :otherMemberId 
-              AND cp1.chatRoom.isGroupChat = 'N'
-            """)
-    Optional<ChatRoom> findExistingPrivateRoom(@Param("memberId") Long memberId,
-                                               @Param("otherMemberId") Long otherMemberId);
+
 }
