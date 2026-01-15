9cd2733ada702e7460afd89d84f89431162456aa
kroad0129 <kroad0129@naver.com>
2026-01-16 04:12:27 +0900

    refactor: 삭제

--- src/main/java/com/highpass/code_review_ide/api/auth/dto/TokenReissueResponse.java
+++
@@ -1,5 +0,0 @@
-package com.highpass.code_review_ide.api.auth.dto;
-
-public record TokenReissueResponse(
-        String accessToken
-) {}
--- src/main/java/com/highpass/code_review_ide/service/chat/dto/ChatMessageResponse.java
+++
@@ -1,8 +0,0 @@
-package com.highpass.code_review_ide.service.chat.dto;
-
-public record ChatMessageResponse(
-        Long roomId,
-        String message,
-        String senderEmail
-) {
-}
