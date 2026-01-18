d7685f53aeb5996a1d4c98518e26b9b0207e88c1
kroad0129 <kroad0129@naver.com>
2026-01-18 19:54:24 +0900

    fix: 필터 등록

--- src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
+++ src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.highpass.code_review_ide.common.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
@@ -20,8 +26,9 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/scalar/**"
                         ).permitAll()
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
