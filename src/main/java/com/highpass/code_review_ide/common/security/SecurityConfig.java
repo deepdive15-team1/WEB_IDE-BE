e05cbd9c0fb42e81b2590683f9249925cf5d4d62
kroad0129 <kroad0129@naver.com>
2026-01-18 13:52:48 +0900

    feat: 시큐리티 Oauth 경로 추가

--- src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
+++ src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.highpass.code_review_ide.common.security;
 
+import com.highpass.code_review_ide.common.security.oauth.CustomOAuth2UserService;
+import com.highpass.code_review_ide.common.security.oauth.OAuth2LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,9 +25,17 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/scalar/**"
+                                "/scalar/**",
+                                "/login/**",
+                                "/oauth2/**"
                         ).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .successHandler(oAuth2LoginSuccessHandler)
                 );
 
         return http.build();
