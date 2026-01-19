b5cefbcf6d097d98b456680b3337bf5001fc3cb5
kroad0129 <kroad0129@naver.com>
2026-01-19 22:17:21 +0900

    fix: cors 수정

--- src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
+++ src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
@@ -71,6 +71,6 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return source; //.
     }
 }
