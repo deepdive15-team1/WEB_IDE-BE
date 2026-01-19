976c9f8a091b77d264c1f9a690335fc01bc72d24
kroad0129 <kroad0129@naver.com>
2026-01-19 21:13:41 +0900

    fix: cors 메소드 명시적으로 수정

--- src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
+++ src/main/java/com/highpass/code_review_ide/common/security/SecurityConfig.java
@@ -59,11 +59,11 @@ public class SecurityConfig {
 
         configuration.setAllowedOriginPatterns(List.of("*"));
 
-        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
 
-        configuration.setExposedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
 
         configuration.setAllowCredentials(true);
 
